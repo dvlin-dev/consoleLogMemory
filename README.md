9a9962fa521230a6a542b88123f4c3926de75fbc
zhangbaolin <baolin.zhang@js.design>
2023-01-31 01:02:34 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # consoleLogMemory
+
+## 参考
+- [Can console.log() cause memory leaks? How to make a browser crash with console.log()?](https://javascript.plainenglish.io/can-console-log-cause-memory-leaks-how-to-make-a-browser-crash-with-console-log-b94e4d248ed8)
+- [千万别让 console.log 上生产！用 Performance 和 Memory 告诉你为什么](https://juejin.cn/post/7185128318235541563#comment)
+- [console.log 一定会导致内存泄漏？不打开 devtools 就不会](https://juejin.cn/post/7185501830040944698#comment)
---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const port = 8888;
+const staticRoot = '.';
+
+app.use(express.static(staticRoot, {
+  etag: true,
+  lastModified: true, 
+  setHeaders(res) {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  }
+}));
+
+http.createServer(app).listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+});
